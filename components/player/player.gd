69c33961d8fc2ffc59bc688d0bd6665ad08fbbb3
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-10-17 20:26:26 +0500

    Finalized Player Movement

--- components/player/player.gd
+++ components/player/player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-@onready var anim_tree: AnimationTree = $AnimationTree
+@onready var anim_tree: AnimationTree = $animation_tree
 @export var anim_speed:= 8.0
+@export var move_speed:= 2000
 
 var pointing_at := 0.0
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var target: float = Input.get_axis("up", "down")
 	pointing_at = lerp(pointing_at, target, anim_speed * delta)
-
 	anim_tree.set("parameters/Move/blend_position", pointing_at)
+
+	velocity.y = move_speed * target * delta * 100
+	move_and_slide()
