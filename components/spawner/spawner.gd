4db143e13d200dfa0e2b46194e1139d2ba78aae1
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-10-19 18:17:25 +0500

    Added Instance limits to spawners
    and cleaners to kill bodies outside the viewport

--- components/spawner/spawner.gd
+++ components/spawner/spawner.gd
@@ -1,17 +1,35 @@
 extends Area2D
 
-@export var item: PackedScene
+enum spawnable_mobs {virus, hacker}
+var virus = preload("res://components/virus/virus.tscn")
+var hacker = preload("res://components/hacker/hacker.tscn")
+
+@export var mob: spawnable_mobs
 @export var region: CollisionShape2D
 @export var interval: float = 1.0
+@export var max_instances: = 2
 @onready var extents = (region.shape as RectangleShape2D).extents
 
 var last_spawn: float = 0.0
+var n: int = 0
 
 func _process(delta: float) -> void:
 	last_spawn += delta
 	if last_spawn > interval:
 		last_spawn = 0
 		var position = to_global(Vector2(randf_range(-extents.x, extents.x), randf_range(-extents.y, extents.y)))
-		var new_item: CharacterBody2D = item.instantiate()
-		new_item.global_position = position
-		add_sibling(new_item)
+		var new_mob: CharacterBody2D
+		if n >= max_instances:
+			return
+		match mob:
+			spawnable_mobs.virus:
+				new_mob = virus.instantiate()
+			spawnable_mobs.hacker:
+				new_mob = hacker.instantiate()
+		n+=1
+		new_mob.global_position = position
+		new_mob.tree_exited.connect(Callable(self, "mob_died"))
+		add_sibling(new_mob)
+
+func mob_died():
+	n -= 1
